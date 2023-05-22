a020816266cb9d7178b59731d1650f5a615d3d27
Daniel Arrais <danielarraiscarvalho@gmail.com>
2023-05-22 07:50:58 -0300

    first commit

---
+++ src/test/java/dev/danielarrais/multiplequeuesrabbit/MultipleQueuesRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danielarrais.multiplequeuesrabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleQueuesRabbitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
